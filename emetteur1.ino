8b588897c470edcbb80ae9f4205cbdb82983cdc9
MR Abdellatif <abdellatif.med@gmail.com>
2019-02-24 03:35:35 +0100

    Add files via upload

---
+++ emetteur1.ino
@@ -0,0 +1,35 @@
+/*
+  433 MHz RF Module Transmitter Demonstration 1
+  RF-Xmit-Demo-1.ino
+  Demonstrates 433 MHz RF Transmitter Module
+  Use with Receiver Demonstration 1
+ 
+  DroneBot Workshop 2018
+  https://dronebotworkshop.com
+*/
+ 
+// Include RadioHead Amplitude Shift Keying Library
+#include <RH_ASK.h>
+// Include dependant SPI Library 
+#include <SPI.h> 
+ 
+// Create Amplitude Shift Keying Object
+RH_ASK rf_driver;
+int led1 = 13;
+ 
+void setup()
+{
+   pinMode(led1, OUTPUT);
+    // Initialize ASK Object
+    rf_driver.init();
+}
+ 
+void loop()
+{
+   digitalWrite(led1, HIGH); 
+    const char *msg = "Welcome to the Workshop!";
+    rf_driver.send((uint8_t *)msg, strlen(msg));
+    rf_driver.waitPacketSent();
+     digitalWrite(led1, LOW); 
+    delay(1000);
+}
